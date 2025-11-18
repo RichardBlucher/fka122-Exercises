51f6b4079f5d129c3ff24b08833f6cd657ce080c
RichardBlucher <98386868+RichardBlucher@users.noreply.github.com>
2025-11-18 11:30:36 +0100

    Add files via upload

---
+++ fka122-Exercises1/library/obj_test/test_main.d
@@ -0,0 +1,4 @@
+obj_test/test_main.o: unit-test/src/test_main.c \
+ unit-test/include/test_main.h \
+ /chalmers/users/blucher/.local/include/check.h \
+ /chalmers/users/blucher/.local/include/check_stdint.h include/tools.h
---
+++ fka122-Exercises1/library/obj_test/tools.d
@@ -0,0 +1,15 @@
+obj_test/tools.o: src/tools.c \
+ /chalmers/users/blucher/.local/include/gsl/gsl_fft_real.h \
+ /chalmers/users/blucher/.local/include/gsl/gsl_math.h \
+ /chalmers/users/blucher/.local/include/gsl/gsl_sys.h \
+ /chalmers/users/blucher/.local/include/gsl/gsl_inline.h \
+ /chalmers/users/blucher/.local/include/gsl/gsl_machine.h \
+ /chalmers/users/blucher/.local/include/gsl/gsl_precision.h \
+ /chalmers/users/blucher/.local/include/gsl/gsl_types.h \
+ /chalmers/users/blucher/.local/include/gsl/gsl_nan.h \
+ /chalmers/users/blucher/.local/include/gsl/gsl_pow_int.h \
+ /chalmers/users/blucher/.local/include/gsl/gsl_minmax.h \
+ /chalmers/users/blucher/.local/include/gsl/gsl_complex.h \
+ /chalmers/users/blucher/.local/include/gsl/gsl_fft.h \
+ /chalmers/users/blucher/.local/include/gsl/gsl_fft_halfcomplex.h \
+ include/tools.h
