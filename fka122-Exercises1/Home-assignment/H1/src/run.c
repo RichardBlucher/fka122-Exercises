b2b9072e740c7beae9672b50205f3db6512b8ac2
lyckhage <lyckhage@chalmers.se>
2025-11-25 10:49:38 +0100

    yolov2

--- fka122-Exercises1/Home-assignment/H1/src/run.c
+++ fka122-Exercises1/Home-assignment/H1/src/run.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "lattice.h"
 #include "potential.h"
+#include "tools.h"
 
 
 int
@@ -14,5 +15,23 @@ run(
     // Write your code here
     // This makes it possible to test
     // 100% of you code
+    const unsigned int n_atoms = 256;
+    const unsigned int N = 4;
+    const double lattice_constant = 1.0;
+
+    double **positions = create_2D_array(n_atoms, 3);
+
+    init_fcc(positions, n_atoms, lattice_constant);
+
+    double potential = 0;
+
+    potential = get_energy_AL(positions, N * lattice_constant, n_atoms);
+
+    printf("Potential energy: %.10f\n", potential);
+    
+
+
+
+
     return 0;
 }
