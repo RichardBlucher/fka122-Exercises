af128971eb3a234aff32f612a3b4f29dad50bd7e
lyckhage <lyckhage@chalmers.se>
2025-12-05 09:35:58 +0100

    task5 done

--- fka122-Exercises1/E2/task4/obj/main.d
+++ fka122-Exercises1/E2/task4/obj/main.d
@@ -1,1 +1,5 @@
-obj/main.o: src/main.c include/tools.h
+obj/main.o: src/main.c include/tools.h \
+ /chalmers/users/lyckhage/.local/include/gsl/gsl_rng.h \
+ /chalmers/users/lyckhage/.local/include/gsl/gsl_types.h \
+ /chalmers/users/lyckhage/.local/include/gsl/gsl_errno.h \
+ /chalmers/users/lyckhage/.local/include/gsl/gsl_inline.h
--- fka122-Exercises1/E2/task4/src/main.c
+++ fka122-Exercises1/E2/task4/src/main.c
@@ -158,5 +158,46 @@ int save_2d_csv(const char *filename, double **data,
 
 int main()
 {
+    double delta = 2.1;
+    const unsigned int n_steps = 100000;
+    double **positions = create_2D_array(n_steps, 3);
+    double *x = malloc(sizeof(double) * 3);
+    int accepted_moves = 0;
+    double **function_values = create_2D_array(n_steps, 2);
+    gsl_rng *k = gsl_rng_alloc(gsl_rng_mt19937);
+    x[0] = 0.0;
+    x[1] = 0.0;
+    x[2] = 0.0;
+    for (unsigned int i = 0; i < n_steps; i++){
+        result_t res = MCMC_step_displace_all(x, delta, k);
+        positions[i][0] = x[0];
+        positions[i][1] = x[1];
+        positions[i][2] = x[2];
+        function_values[i][0] = res.function_value;
+        function_values[i][1] = res.probability;
+        accepted_moves += res.accepted;
+    }
+    printf("Acceptance ratio: %.5f\n", (double)accepted_moves / n_steps);
+    double sum = 0.0;
+    double sum_sq = 0.0;
+    for(unsigned int i = 1000; i < n_steps; i++){
+        sum += function_values[i][0];
+        sum_sq += function_values[i][0] * function_values[i][0];
+    }
+    double mean = sum / n_steps;
+    double variance = sum_sq / n_steps - (mean*mean);
+    double error = sqrt(variance / n_steps);
+    printf("Estimated integral: %.5f +/- %.5f\n", mean, error);
+
+
+    //save_2d_csv("positions.csv", positions, n_steps, 3);
+    //save_2d_csv("function_values.csv", function_values, n_steps, 2);
+
+    destroy_2D_array(positions, n_steps);
+    destroy_2D_array(function_values, n_steps);
+    gsl_rng_free(k);
+    free(x);
+
+
     return 0;
 }
