0468509065fcd8f2b498e13dc991c3c45ce39d57
lyckhage <lyckhage@chalmers.se>
2025-12-05 10:07:24 +0100

    task6 done

--- fka122-Exercises1/E2/task6/src/main.c
+++ fka122-Exercises1/E2/task6/src/main.c
@@ -1,137 +1,103 @@
-
-
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include "tools.h"
-#include <gsl/gsl_rng.h>
-
 //#include "attempt.h"
-#define PI 3.141592653589
+
 
 /*
- * The following struct is defined in the header file
- * Use it to store the result
+ * The following functions are to be used:
 */
-typedef struct{
-    double probability;
-    double function_value;
-    int accepted;
-} result_t;
+/*
+double
+average(
+        double *v,
+        unsigned int len
+       );
+double
+variance(
+         double *v,
+         unsigned int len
+        );
+*/
 
 
 /* ***************************************
 *
-* Perfmore the calculation of the
-* unormalized probability function, i.e.,
-* the weight used in the MCMC routine
+* Autocorrelation
 *
 * Parameters
 * ----------
-*  x - current walker position, size = 3
+*  data - the raw data from which the
+*         autocorrelation should be
+*         calculated
+*  data_len     - the len of data
+*  time_lag_ind - the lag (index) at 
+*                 which the autocorrelation
+*                 should be calculated
 *
 * Returns
 * -------
-* The weight of the current position
+*  The autocorrelation at a specific time
+*  lag
 *
 * ***************************************/
-double weight(
-              double *x)
+double autocorrelation(
+			   double *data,
+			   int data_len,
+			   int time_lag_ind)
 {
-    double exponent = (x[0]*x[0] + x[1]*x[1] + x[2]*x[2]);
-    double weight_value = exp(-exponent);
-    return weight_value;
+    double ret = 0;
+    double fi_average = average(data, data_len);
+    double new_data[data_len];
+    for(unsigned int i = 0; i < data_len - time_lag_ind; i++){
+        new_data[i]= data[i] * data[i + time_lag_ind];
+    }
+    double new_data_average = average(new_data, data_len - time_lag_ind);
+    double data_variance = standard_deviation(data, data_len);
+    ret = (new_data_average - fi_average * fi_average) / (data_variance*data_variance);
+
+
+    return ret;
 }
 
 /* ***************************************
 *
-* Perfmore the calculation of the
-* function to be sampled
+* Block average
 *
 * Parameters
 * ----------
-*  x - current walker position, size = 3
+*  data       - the raw data from which the
+*               autocorrelation should be
+                calculated
+*  data_len   - the length of data
+*  block_size - the size of the block
 *
 * Returns
 * -------
-* The function value
+* The statistical inefficency for a given
+* block size
 *
 * ***************************************/
-double function(
-                double *x)
+double block_average(double *data,
+	                int data_len,
+	                int block_size
+	               )
 {
-    double function_value = x[0]*x[0] + x[1]*x[1]*x[0]*x[0] + x[2]*x[2]*x[0]*x[0]*x[1]*x[1];
-    return function_value;
+    double ret = 0;
+    int n_blocks = data_len / block_size;
+    double block_averages[n_blocks];
+    for(unsigned int i = 0; i < n_blocks; i++){
+        double sum = 0.;
+        for(unsigned int j = 0; j < block_size; j++){
+            sum += data[i*block_size + j];
+        }
+        block_averages[i] = sum / block_size;
+    }
+    ret = standard_deviation(block_averages, n_blocks)*standard_deviation(block_averages , n_blocks) / (standard_deviation(data, data_len)*standard_deviation(data, data_len));
+
+    return ret;
 }
-
-/* ***************************************
-*
-* Perfmore the calculation of the
-* function to be sampled
-*
-* - Use gsl_rng_uniform to displace the
-*   walker.
-* - The walker should be displaced
-*   in the order x,y,z.
-* - The draw for the acceptance condition
-*   should be done after the draws
-*   for the displacing of the walker
-*
-*
-* Parameters
-* ----------
-*  x - current walker position, size = 3
-*  delta - step size
-*  k - GSL random number generator object
-*
-* Returns
-* -------
-* - The function should update the x parameter
-*   to reflect if the move was accepted or
-*   rejected.
-* - result should contain the probability
-*   of the "exiting" x parameter.
-* - result should should contain the function
-*   value of the "exiting" x parameter.
-* - result should be 1 if the move was accepted
-*   and 0 if it was rejected.
-*
-* ***************************************/
- result_t MCMC_step_displace_all(
-                                 double *x,
-                                 double delta,
-                                 gsl_rng *k)
-{
-    result_t result; 
-    double x_new[3];
-    for (int i = 0; i < 3; i++){
-        double r = gsl_rng_uniform(k);
-        x_new[i] = x[i] + (r - 0.5) * delta;
-    }
-    double pm = weight(x);
-    double pt = weight(x_new);
-
-    double q = pt/pm;
-    double epsilon = gsl_rng_uniform(k);
-    if (epsilon < q){
-        result.accepted = 1;
-        x[0] = x_new[0];
-        x[1] = x_new[1];
-        x[2] = x_new[2];
-    }
-    else{
-        result.accepted = 0;
-    }
-
-
-    result.probability = weight(x);
-    result.function_value = function(x);
-    //result.accepted = 0;
-
-    return result;
-}
-
-
 
 
 int save_2d_csv(const char *filename, double **data,
@@ -158,46 +124,6 @@ int save_2d_csv(const char *filename, double **data,
 
 int main()
 {
-    double delta = 2.1;
-    const unsigned int n_steps = 100000;
-    double **positions = create_2D_array(n_steps, 3);
-    double *x = malloc(sizeof(double) * 3);
-    int accepted_moves = 0;
-    double **function_values = create_2D_array(n_steps, 2);
-    gsl_rng *k = gsl_rng_alloc(gsl_rng_mt19937);
-    x[0] = 0.0;
-    x[1] = 0.0;
-    x[2] = 0.0;
-    for (unsigned int i = 0; i < n_steps; i++){
-        result_t res = MCMC_step_displace_all(x, delta, k);
-        positions[i][0] = x[0];
-        positions[i][1] = x[1];
-        positions[i][2] = x[2];
-        function_values[i][0] = res.function_value;
-        function_values[i][1] = res.probability;
-        accepted_moves += res.accepted;
-    }
-    printf("Acceptance ratio: %.5f\n", (double)accepted_moves / n_steps);
-    double sum = 0.0;
-    double sum_sq = 0.0;
-    for(unsigned int i = 1000; i < n_steps; i++){
-        sum += function_values[i][0];
-        sum_sq += function_values[i][0] * function_values[i][0];
-    }
-    double mean = sum / n_steps;
-    double variance = sum_sq / n_steps - (mean*mean);
-    double error = sqrt(variance / n_steps);
-    printf("Estimated integral: %.5f +/- %.5f\n", mean, error);
-
-
-    //save_2d_csv("positions.csv", positions, n_steps, 3);
-    //save_2d_csv("function_values.csv", function_values, n_steps, 2);
-
-    destroy_2D_array(positions, n_steps);
-    destroy_2D_array(function_values, n_steps);
-    gsl_rng_free(k);
-    free(x);
-
 
     return 0;
 }
