6b56c27cbed5fe5aa5f9c168a54855c52b75b96d
lyckhage <lyckhage@chalmers.se>
2025-12-05 08:55:09 +0100

    task4 complete

--- fka122-Exercises1/E2/task4/src/main.c
+++ fka122-Exercises1/E2/task4/src/main.c
@@ -4,9 +4,132 @@
 #include <stdlib.h>
 #include <math.h>
 #include "tools.h"
+#include <gsl/gsl_rng.h>
+
+//#include "attempt.h"
+#define PI 3.141592653589
+
+/*
+ * The following struct is defined in the header file
+ * Use it to store the result
+*/
+typedef struct{
+    double probability;
+    double function_value;
+    int accepted;
+} result_t;
 
 
+/* ***************************************
+*
+* Perfmore the calculation of the
+* unormalized probability function, i.e.,
+* the weight used in the MCMC routine
+*
+* Parameters
+* ----------
+*  x - current walker position, size = 3
+*
+* Returns
+* -------
+* The weight of the current position
+*
+* ***************************************/
+double weight(
+              double *x)
+{
+    double exponent = (x[0]*x[0] + x[1]*x[1] + x[2]*x[2]);
+    double weight_value = exp(-exponent);
+    return weight_value;
+}
 
+/* ***************************************
+*
+* Perfmore the calculation of the
+* function to be sampled
+*
+* Parameters
+* ----------
+*  x - current walker position, size = 3
+*
+* Returns
+* -------
+* The function value
+*
+* ***************************************/
+double function(
+                double *x)
+{
+    double function_value = x[0]*x[0] + x[1]*x[1]*x[0]*x[0] + x[2]*x[2]*x[0]*x[0]*x[1]*x[1];
+    return function_value;
+}
+
+/* ***************************************
+*
+* Perfmore the calculation of the
+* function to be sampled
+*
+* - Use gsl_rng_uniform to displace the
+*   walker.
+* - The walker should be displaced
+*   in the order x,y,z.
+* - The draw for the acceptance condition
+*   should be done after the draws
+*   for the displacing of the walker
+*
+*
+* Parameters
+* ----------
+*  x - current walker position, size = 3
+*  delta - step size
+*  k - GSL random number generator object
+*
+* Returns
+* -------
+* - The function should update the x parameter
+*   to reflect if the move was accepted or
+*   rejected.
+* - result should contain the probability
+*   of the "exiting" x parameter.
+* - result should should contain the function
+*   value of the "exiting" x parameter.
+* - result should be 1 if the move was accepted
+*   and 0 if it was rejected.
+*
+* ***************************************/
+ result_t MCMC_step_displace_all(
+                                 double *x,
+                                 double delta,
+                                 gsl_rng *k)
+{
+    result_t result; 
+    double x_new[3];
+    for (int i = 0; i < 3; i++){
+        double r = gsl_rng_uniform(k);
+        x_new[i] = x[i] + (r - 0.5) * delta;
+    }
+    double pm = weight(x);
+    double pt = weight(x_new);
+
+    double q = pt/pm;
+    double epsilon = gsl_rng_uniform(k);
+    if (epsilon < q){
+        result.accepted = 1;
+        x[0] = x_new[0];
+        x[1] = x_new[1];
+        x[2] = x_new[2];
+    }
+    else{
+        result.accepted = 0;
+    }
+
+
+    result.probability = weight(x);
+    result.function_value = function(x);
+    //result.accepted = 0;
+
+    return result;
+}
 
 
 
