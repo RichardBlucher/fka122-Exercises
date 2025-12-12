7dd360322516cc34d0d66a456af330434103bb2b
lyckhage <lyckhage@chalmers.se>
2025-12-12 09:21:30 +0100

    start e3

---
+++ fka122-Exercises1/E3/task1/src/main.c
@@ -0,0 +1,34 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "tools.h"
+
+
+
+int save_2d_csv(const char *filename, double **data,
+                unsigned int rows, unsigned int cols)
+{
+    FILE *fp = fopen(filename, "w");
+    if (!fp) {
+        perror("fopen");
+        return -1;
+    }
+
+    for (unsigned int i = 0; i < rows; ++i) {
+        for (unsigned int j = 0; j < cols; ++j) {
+            fprintf(fp, "%.10e", data[i][j]);
+            if (j + 1 < cols) fprintf(fp, ",");
+        }
+        fprintf(fp, "\n");
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+
+int main()
+{
+   
+    return 0;
+}
